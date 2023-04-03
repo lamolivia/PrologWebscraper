b75eb615ebffc793c74bfb74b82cfe7209a3424f
lamolivi <olivia.lam@alumni.ubc.ca>
2023-04-03 15:34:15 -0700

    extracting dates

--- scrape_312.pl
+++ scrape_312.pl
@@ -5,16 +5,51 @@
 
 url("https://www.cs.ubc.ca/~poole/cs312/2023/schedule.html").
 
+% there is def a re_match("Mar 1")
+
 download_page(URL, HTML) :-
     http_open(URL, In, []),
     load_html(In, HTML, []),
     close(In).
 
+parse_dates(Text, Month, Day) :-
+    re_matchsub("^.*?(Jan|Feb|Mar|April|May|June|July|Aug|Sept|Oct|Nov|Dec)[a-z]*[^0-9]+([0-9]+).*?", Text, Matches),
+    print(Matches),
+    dict_pairs(Matches, _, Pairs),
+    memberchk(1-Month, Pairs),
+    memberchk(2-Day, Pairs),
+    %print(Month),
+    %print(Day), 
+    month_number(Month, _),
+    atom_number(Day, DayNum).
+    day_number(DayNum). 
+
+day_number(D) :-
+    between(0, 31, D).
+
+month_number("Jan", 1).
+month_number("Feb", 2).
+month_number("Mar", 3).
+month_number("March", 3).
+month_number("April", 4).
+month_number("May", 5).
+month_number("June", 6).
+month_number("July", 7).
+month_number("Aug", 8).
+month_number("Sept", 9).
+month_number("Oct", 10).
+month_number("Nov", 11).
+month_number("Dec", 12).
+
 extract_data(HTML, Data) :-
-    findall(Value, (
-        xpath(HTML, //ul, UL),
+    findall(Date, (
+        xpath(HTML, //(ul), UL),
         xpath(UL, //li(text), TextNode),
-        atom_string(Value, TextNode)
+        atom_string(Value, TextNode),
+        %print(Value),
+        parse_dates(Value, MonthNumber, Day),
+        %print(MonthNumber),
+        format(atom(Date), '~w ~w', [MonthNumber, Day])
     ), DataValues),
     maplist(atom_string, DataValues, Data).
 
