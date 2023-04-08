45f168f9bd5d012f94966562faafd7e5d5fc3af7
fr1tzley <imbusyreading09@gmail.com>
2023-04-07 21:52:11 -0700

    horrible spicy....ohjrrible horrible spicy....

--- scheduling.pl
+++ scheduling.pl
@@ -23,7 +23,7 @@ read_lines(Stream, [Line|Rest]) :-
 
 
 
-
+% MAP FIND COMPONENTS
 % converts the names of the courses into their components by searching components in the passed data.
 map_find_components([], Data, L, L).
 map_find_components([H|T], Data, L, L2) :-
@@ -31,38 +31,49 @@ map_find_components([H|T], Data, L, L2) :-
     map_find_components(T, Data, L, L1),
     find_components(H, Data, H1).
 
+% FIND COMPONENTS 
 % searches for a class with the given name, and returns a list of its components.
 find_components(_, [], []).
 find_components(Name, [class(Name, Components)|_], Components).
 find_components(Name, [_|R], C) :- find_components(Name, R, C).
 
+% MAKE SCHEDULE
 % creates a schedule for the user using the list of comopnents provided. with each component, it will check if a valid schedule can be made using the
 % first set of durations. if so, that schedule will be used. if not, it will search through the remaining durations.
 
 % if the list of components is empty, return the existing schedule.
 make_schedule([], S, S).
+
 % if a valid schedule can be made with the current durations, make the registration,
 % and append it to the schedule returned by recursion.
-make_schedule([component(Name, Type, [D|_])|R], S, [R|S1]) :-
-    R is registration(Name, Type, D),
-    remove_overlaps_list(D, R, []),
-    make_schedule(R, S, S1).
+make_schedule([component(Name, Type, [D|_])|R], S, [RG|S1]) :-
+    RG = registration(Name, Type, D),
+    make_schedule(R1, S, S1),
+    remove_overlaps_list(D, R, [], R1).
+
 % if no valid schedule can be made, try the next durations
 make_schedule([component(Name, Type, [_|D1])|R], S, S1) :-
     make_schedule([component(Name, Type, D1)|R], S, S1).
+
 % if a component is out of durations, do not register it.
 make_schedule([component(Name, Type, [])|R], S, S1) :-
     make_schedule(R, S, S1).
 
+% REMOVE OVERLAPS LIST
 % given a list of components, remove every duration for each component that overlaps with the given list of durations, DL.
-remove_overlaps_list(DL, [], L, L).
+remove_overlaps_list(_, [], L, L).
+
+% if a component has no possible durations to register left, it is removed from the list of components to be registered
 remove_overlaps_list(DL, [component(Name, Type, D)|R], L, L1) :-
     remove_overlaps_durs(DL, D, []),
     remove_overlaps_list(DL, R, L, L1).
+
+% recreate the component with invalid durations deleted, and append to the list from the recursive call
 remove_overlaps_list(DL, [component(Name, Type, D)|R], L, [component(Name, Type, D1)|L1]) :-
     remove_overlaps_durs(DL, D, D1),
     remove_overlaps_list(DL, R, L, L1).
 
+% START SCHEDULING
 % the main function to make a schedule.
 % the function takes a list of class names, and creates a schedule for the student including all of those classes.
 start_scheduling(Classes, Data, S) :-
--- scheduling.plt
+++ scheduling.plt
@@ -72,9 +72,9 @@ test(remove_overlaps_list) :-
     DT1 = duration(T1, T2, T),
     DT2 = duration(T3, T4, T),
 
-    C1 = (CS312, Lecture, [[DM1, DW1, DF1], [DM2, DW2, DW3]]),
-    C2 = (CS313, Lecture, [[DM1, DW2, DF1]]),
-    C3 = (CS313, Lab, [[DT1], [DT2]]),
+    C1 = component(CS312, Lecture, [[DM1, DW1, DF1], [DM2, DW2, DW3]]),
+    C2 = component(CS313, Lecture, [[DM1, DW2, DF1]]),
+    C3 = component(CS313, Lab, [[DT1], [DT2]]),
 
     CR1 = component(CS312, Lecture, [[DM1, DW1, DF1]]),
 
@@ -85,6 +85,44 @@ test(remove_overlaps_list) :-
     R1 = [CR1, C3], 
     
     remove_overlaps_list(DL, L1, [], R1).
+
+test(make_schedule) :-
+    T1 = clock_time(10,00),
+    T2 = clock_time(10,50),
+    T3 = clock_time(11,00),
+    T4 = clock_time(11,50),
+    T5 = clock_time(12,00),
+
+    DM1 = duration(T1, T2, M),
+    DW1 = duration(T1, T2, W),
+    DF1 = duration(T1, T2, F),
+
+    DM2 = duration(T3, T4, M),
+    DW2 = duration(T3, T4, W),
+    DF2 = duration(T3, T4, F),
+
+    DT1 = duration(T1, T2, T),
+    DT2 = duration(T3, T4, T),
+
+    C1 = component(CS312, Lecture, [[DM1, DW1, DF1], [DM2, DW2, DW3]]),
+    C2 = component(CS313, Lecture, [[DM1, DW2, DF1]]),
+    C3 = component(CS313, Lab, [[DT1], [DT2]]),
+
+    RG1 = registration(CS312, Lecture, [DM2, DW2, DW3]),
+    RG2 = registration(CS313, Lecture, [DM1, DW2, DF1]),
+    RG3 = registration(CS313, Lab, [DT1]),
+
+    R1 = [RG1, RG2, RG3],
+
+    make_schedule([C1, C2, C3], [], R1),
+
+    C4 = component(CS210, Lecture, [[DM1, DW2, DF1]]),
+    
+    RG4 = registration(CS313, Lecture, [DM1, DW2, DF1]),
+
+    R2 = [RG4],
+
+    make_schedule([C4], [], R2).
 
 
 :- end_tests(scheduling).
