cc95b3e779abe37435fa87c6a6faa8920486c7a1
William Gervasio <wilpgerv@telus.net>
2023-04-10 19:56:10 -0700

    fixed parsing hopefull

--- scrape_course.pl
+++ scrape_course.pl
@@ -28,14 +28,38 @@ download_page(URL, HTML) :-
     close(In).
 
 
-extract_data(Html, CourseName, Sections) :-
-    xpath(Html, //table(contains(@class,'section-summary')), Table),
-    findall(component(CourseName, Type, [[section(SectionName, Start, End, Day,  Term)]]), (
-        extract_section(Table, Type, Start,End,Day,SectionName, Term)
-    ), Sections).
 
-to_clock_time(Text, Time) :-
-    split_string(Text, ":", "", Res).
+remove_type_from_section(section(SectionName, Start, End, Day, Term, _), section(SectionName, Start, End, Day, Term)).
+
+make_component(Sections, CourseName, Type, component(CourseName, Type, UnTypeSections)) :-
+    include(match_type(Type), Sections, TypeSections),
+    maplist(remove_type_from_section, TypeSections, UnTypeSections).
+
+match_type(IntendedType, section(_, _, _, _, _, Type)) :-
+    IntendedType = Type.
+
+find_types(Sections, Types) :-
+    findall(Type, (
+        member(section(_, _, _, _, _, Type), Sections),
+        Type \= ""
+    ), DupeTypes),
+    list_to_set(DupeTypes,Types).
+
+extract_data(Html, CourseName, Components) :-
+    xpath(Html, //table(contains(@class,'section-summary')), Table),
+    % [component(CPSC 001,Lecture,[[section(clock_time(15,00),clock_time(16,00),Mon,CPSC 100 101,1)]])],
+    findall(section(SectionName, Start, End, Day,  Term, Type), (
+        extract_section(Table, Type, Start,End,Day,SectionName, Term)
+    ), UnsortedSections),
+    find_types(UnsortedSections, Types),
+    maplist(make_component(UnsortedSections, CourseName), Types, Components).
+    
+
+
+to_clock_time(Text, clock_time(Hrs, Mins)) :-
+    split_string(Text, ":", "", [StrHrs,StrMins]),
+    number_string(Hrs, StrHrs),
+    number_string(Mins, StrMins).
 
 extract_section(Table, Type, Start, End, Day, SectionName, Term) :-
     xpath(Table, //tr(contains(@class,'section')), Section),
@@ -46,12 +70,19 @@ extract_section(Table, Type, Start, End, Day, SectionName, Term) :-
     re_matchsub("Mon|Tue|Wed|Thu|Fri|Sat|Sun", Days, ReDay),
     dict_pairs(ReDay, _, Pairs),
     memberchk(_-Day, Pairs),
-    xpath(Section, //td(8,text(string)), Start),
-    xpath(Section, //td(9,text(string)), End).
+    xpath(Section, //td(8,text(string)), StartString),
+    to_clock_time(StartString, Start),
+    xpath(Section, //td(9,text(string)), EndString),
+    to_clock_time(EndString, End).
 
 
-save_to_file(Data) :-
+save_scrape_to_file(Data) :-
     open('output.txt', write, Out),
+    write(Out, Data),
+    close(Out).
+
+save_sched_to_file(Data) :-
+    open('schedule.txt', write, Out),
     write(Out, Data),
     close(Out).
 
@@ -62,12 +93,13 @@ run_web_scraper :-
     split_string(Input, ",", "", ClassList),
     maplist(web_scraper_helper, ClassList, ListOfCourseDataLists),
     foldl(append, ListOfCourseDataLists, [], CourseData),
-    save_to_file(CourseData),
-    %  this line causes stack overflows
-    Example = [component(CPSC 001,Lecture,[[section(clock_time(15,00),clock_time(16,00),Mon,CPSC 100 101,1)]])],
-    start_scheduling(ClassList, Example, Schedule),
-    writeln(Schedule),
-    writeln("finished"),
+    %  debugging purposes
+    save_scrape_to_file(CourseData),
+    % Example = [component(CPSC 001,Lecture,[[section(clock_time(15,00),clock_time(16,00),Mon,CPSC 100 101,1)]])],
+    start_scheduling(ClassList, CourseData, Schedule),
+    %  debugging purposes
+    save_sched_to_file(Schedule),
+    % writeln("finished"),
     halt.
 
 web_scraper_helper(DeptCourseNum, CourseData) :-
