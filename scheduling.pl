260a06eb3a05515bd9d8d402c2b30953c8c0b19e
William Gervasio <wilpgerv@telus.net>
2023-04-10 23:57:37 -0700

    finished stuf

--- scheduling.pl
+++ scheduling.pl
@@ -1,46 +1,65 @@
-:- module(scheduling).
+:- module(scheduling_wg).
 :- include(scheduleutils).
 
-% MAKE SCHEDULE
-% creates a schedule for the user using the list of comopnents provided. with each component, it will check if a valid schedule can be made using the
-% first set of sections. if so, that schedule will be used. if not, it will search through the remaining sections.
-
-% if the list of components is empty, return the existing schedule.
-make_schedule([], S, S).
-
-% if a valid schedule can be made with the current sections, make the registration,
-% and append it to the schedule returned by recursion.
-make_schedule([component(Name, Type, [Sec|_])|R], S, [RG|S1]) :-
-    RG = registration(Name, Type, Sec),
-    make_schedule(R, S, S1).
-
-% if no valid schedule can be made, try the next sections
-make_schedule([component(Name, Type, [_|Rest])|R], S, S1) :-
-    make_schedule([component(Name, Type, Rest)|R], S, S1).
-
-% if a component is out of sections to try, do not register it.
-make_schedule([component(Name, Type, [])|R], S, S1) :-
-    make_schedule(R, S, S1).
-
-% clause if no other rules apply to just return existing schedule
-make_schedule(_, S, S).
-    
-check_overlap(_, []) :- true.
-check_overlap(section(_, _, [H|T], ))
-
-% REMOVE OVERLAPS LIST
-% given a list of components, remove every section for each component that overlaps with the given list of sections, SL. This is not used because we 
-% could not get it to work :P
-remove_overlaps_list(_, [], L, L).
-
-% recreate the component with invalid sections deleted, and append to the list from the recursive call
-remove_overlaps_list(SL, [component(Name, Type, Sec)|R], L, [component(Name, Type, Sec1)|L1]) :-
-    remove_overlaps_list(SL, R, L, L1),
-    remove_overlaps_secs(SL, Sec, Sec1).
 
 
-% START SCHEDULING
-% the main function to make a schedule.
-% the function takes a list of class names, and creates a schedule for the student including all of those classes.
-start_scheduling(Classes, Data, S) :-
-    make_schedule(Data, [], S).
+
+
+
+
+
+
+
+% Function to sort sections in a component by ending clock time
+sort_sections([component, Course, Type, Sections], [component, Course, Type, SortedSections]) :-
+    predsort(compare_ending_time, Sections, SortedSections).
+
+% Comparison predicate to compare two sections based on their ending clock time and day
+compare_ending_time(Order, section(_, _, EndTime1, DayAtom1, _), section(_, _, EndTime2, DayAtom2, _)) :-
+    day_to_number(DayAtom1, Day1),
+    day_to_number(DayAtom2, Day2),
+    compare(OrderDay, Day1, Day2),
+    (OrderDay == 0 ->
+        compare(OrderTime, EndTime1, EndTime2),
+        Order = OrderTime;
+        Order = OrderDay
+    ).
+
+% Function to convert day name to number for comparison
+day_to_number(Mon, 1).
+day_to_number(Tue, 2).
+day_to_number(Wed, 3).
+day_to_number(Thu, 4).
+day_to_number(Fri, 5).
+day_to_number(Sat, 6).
+day_to_number(Sun, 7).
+
+
+% Function to sort components based on number of sections
+sort_components(ComponentList, SortedComponentList) :-
+    predsort(compare_sections, ComponentList, SortedComponentList).
+
+% Comparison predicate to compare two components based on number of sections
+compare_sections(Order, component(_,_,Sections1), component(_,_,Sections2)) :-
+    length(Sections1, Length1),
+    length(Sections2, Length2),
+    compare(Order, Length1, Length2).
+%  try break ties on earliest if possible but dont bother if impossible
+
+
+add_schedule([component(Name, Type, [Section|Sections])|Rest], Rest, Schedule, [Section|Schedule]) :-
+    no_overlap(Section, Schedule).
+
+add_schedule([component(Name, Type, [Section|Sections])|Rest], [component(Name, Type, Sections)|Rest], Schedule, Schedule) :-
+    not(no_overlap(Section, Schedule)).
+
+do_schedule([], Schedule, Schedule).
+do_schedule(Components, Schedule, NewSchedule) :-
+    add_schedule(Components, ResComps, Schedule, ResSched),
+    sort_components(ResComps, SortedComps),
+    do_schedule(SortedComps, ResSched, NewSchedule).
+
+
+start_scheduling(Classes, Data, Schedule) :-
+    sort_components(Data, SortedCompLen),
+    do_schedule(SortedCompLen, [], Schedule).
